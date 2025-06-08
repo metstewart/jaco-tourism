c971971f84dec97adac6f17a2e33f43fa5a079a5
Megan Stewart <147526987+megan-estewart@users.noreply.github.com>
2025-06-07 19:07:54 -0500

    Updates to point of interest selection; Minor adjustments to location lat and longs

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import os
 from flask import Flask, render_template
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    api_key = os.getenv("GOOGLE_MAPS_API_KEY")
+    return render_template("index.html", api_key=api_key)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
